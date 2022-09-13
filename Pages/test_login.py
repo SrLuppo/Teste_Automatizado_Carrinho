1a2e026003dc1ab8c0caeb36f8edb8d2d998f857
SrLuppo <92128439+SrLuppo@users.noreply.github.com>
2022-09-13 18:00:08 -0300

    Automação_Carrinho

---
+++ Pages/test_login.py
@@ -0,0 +1,35 @@
+from selenium.webdriver import Chrome
+from Pages.Data import Login, Filter, SeeCart, Checkout, Final
+import time
+
+browser = Chrome()
+inicio = Login(browser)
+inicio.signin()
+
+time.sleep(3)
+
+ordenar = Filter(browser)
+ordenar.filtro()
+
+time.sleep(3)
+
+ordenar.addcarrinho()
+
+time.sleep(3)
+
+carrinho = SeeCart(browser)
+carrinho.cart()
+
+time.sleep(3)
+
+confirma = Checkout(browser)
+confirma.info()
+
+time.sleep(3)
+
+saida = Final(browser)
+saida.complete()
+
+
+print('Compras realizadas com sucesso!')
+
---
+++ README.md
@@ -0,0 +1,13 @@
+# Projeto_QA
+
+Descrição: Testar o carrinho de compras
+
+Passos: Entrar com login e senha
+        inserir produto no carrinho
+        
+Resultado esperado: entrar no site e inserir o produto no carrinho e realizar a compra
+
+
+
+
+
